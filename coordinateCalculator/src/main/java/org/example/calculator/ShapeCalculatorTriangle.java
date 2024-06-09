4b1744957441fb48c1ca482d95e7d7287170a9ce
Halo <87004079+Halo-oo@users.noreply.github.com>
2024-06-09 12:54:51 +0900

    feat: 삼각형 넓이 계산 로직 구현

--- coordinateCalculator/src/main/java/org/example/calculator/ShapeCalculatorTriangle.java
+++ coordinateCalculator/src/main/java/org/example/calculator/ShapeCalculatorTriangle.java
@@ -10,7 +10,29 @@ public class ShapeCalculatorTriangle implements ShapeCalculator {
     @Override
     public CalculationResult calculationShape(List<Point> coordinateList) {
         CalculationResult calculationResult = new CalculationResult();
-        System.out.println("## 삼각형 넓이 구하기");
+        calculationResult.setShapeType("삼각형의 넓이");
+
+        double result = calculatingTriangleArea(coordinateList.get(0), coordinateList.get(1), coordinateList.get(2));
+        calculationResult.setCalculatorResult(result);
+
         return calculationResult;
+    }
+
+    /**
+     * 삼각형 넓이 계산
+     */
+    public double calculatingTriangleArea(Point firstPoint, Point secondPoint, Point thirdPoint) {
+        Point point = firstPoint;
+
+        // 세 변의 길이 계산
+        double aSideLength = point.calculatingDistanceTwoPoints(firstPoint, secondPoint);
+        double bSideLength = point.calculatingDistanceTwoPoints(secondPoint, thirdPoint);
+        double cSideLength = point.calculatingDistanceTwoPoints(thirdPoint, firstPoint);
+
+        // 삼각형 넓이
+        double halfCircumference = (aSideLength + bSideLength + cSideLength) / 2;
+        double area = Math.sqrt(halfCircumference * (halfCircumference-aSideLength) * (halfCircumference-bSideLength) * (halfCircumference-cSideLength));
+
+        return area;
     }
 }
